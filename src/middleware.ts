20655701b9000227b5d6faf8e889e82b5065a9be
Toprak Koç <hello@toprak.dev>
2023-09-19 23:37:54 +0300

    updated middle ware public routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/"],
+  publicRoutes: ["/", "/api/trpc/posts.getAll"],
 });
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/(.*?trpc.*?|(?!static|.*\\..*|_next|favicon.ico).*)", "/"],
 };
